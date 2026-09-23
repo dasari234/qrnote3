17e823f960caa614cd8028a8a1b48cd2a85c2499
Srinivasa Rao Dasari <sridasari@innominds.com>
2026-09-23 09:55:31 +0530

    transparent bg

--- app/icon.tsx
+++ app/icon.tsx
@@ -15,7 +15,7 @@ export default function Icon() {
       <div
         style={{
           fontSize: 24,
-          background: '#fff',
+          // background: '#fff',
           width: '100%',
           height: '100%',
           display: 'flex',
